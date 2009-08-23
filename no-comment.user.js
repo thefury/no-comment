4ded90397ec50e44925707b28e004951f2c0a9de
Trevor Oke <trevoroke@trevor-okes-mac-mini.local>
2009-08-22 23:03:53 -0400

    Added code for Toronto Star comments fixed code for CBC comments renamed functions for consistency

--- no-comment.user.js
+++ no-comment.user.js
@@ -1,18 +1,57 @@
 // ==UserScript==
-// @name          No Comment!
-// @namespace     http://download.trevoroke.com/userscripts/
-// @description   Remove media comments
-// @include       http://*.cbc.ca/*
-// @include       http://cbc.ca/*
+// @name No Comment!
+// @namespace http://download.trevoroke.com/userscripts/
+// @description Remove media comments from the CBC and the Toronto Star
+// @include http://*.cbc.ca/*
+// @include http://cbc.ca/*
+// @include http://*.thestar.com/*
+// @include http://thestar.com/*
+
 // ==/UserScript==
 
-function  clobber_element(elem)
+// grabbed from the Greasemonkey docs
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+
+// grabbed from somewhere online
+function getElementsByClassName(cl) {
+    var retnode = [];
+    var myclass = new RegExp('\\b'+cl+'\\b');
+    var elem = this.getElementsByTagName('*');
+    for (var i = 0; i < elem.length; i++) {
+        var classes = elem[i].className;
+        if (myclass.test(classes)) retnode.push(elem[i]);
+    }
+    return retnode;
+};
+
+function clobberElement(elem)
 {
     if (elem) {
-        elem.parentNode.removeChild(elem);
+        textElem = document.createTextNode('');
+        elem.parentNode.replaceChild(textElem, elem);
     }
 }
 
-clobber_element(document.getElementById('socialtools'));
-clobber_element(document.getElementById('socialcomments'));
-clobber_element(document.getElementById('socialhead'));
+// CBC
+addGlobalStyle('.d-inline  { visibility: hidden; display: none; }');
+
+clobberElement(document.getElementById('socialtools'));
+clobberElement(document.getElementById('socialcomments'));
+clobberElement(document.getElementById('socialhead'));
+
+// Toronto Star
+clobberElement(document.getElementById('usercomment_wrapper'));
+clobberElement(document.getElementById('aspnetForm'));
+
+elems = document.getElementsByClassName('CommentsOnStory');
+for (i=0; i<elems.length; i++)
+    clobberElement(elems[i]);
+
